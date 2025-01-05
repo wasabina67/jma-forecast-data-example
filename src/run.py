fd8af06cf3be97090a98f3ecf488a2a8a4c1591e
wasabina67 <wasabina67@gmail.com>
2025-01-05 13:25:00 +0900

    Update src/run.py

--- src/run.py
+++ src/run.py
@@ -6,6 +6,14 @@ def main():
     overview_forecast_url = f"https://www.jma.go.jp/bosai/forecast/data/overview_forecast/{code}.json"
     forecast_url = f"https://www.jma.go.jp/bosai/forecast/data/forecast/{code}.json"
 
+    resp = requests.get(overview_forecast_url)
+    resp_json = resp.json()
+    print(resp_json)
+
+    resp = requests.get(forecast_url)
+    resp_json = resp.json()
+    print(resp_json)
+
 
 if __name__ == "__main__":
     main()
