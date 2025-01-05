4d450928ee87549801b5374b36e45b2cb32fa37f
wasabina67 <wasabina67@gmail.com>
2025-01-05 13:25:42 +0900

    Update src/run.py

--- src/run.py
+++ src/run.py
@@ -3,7 +3,9 @@ import requests
 
 def main():
     code = "130000"
-    overview_forecast_url = f"https://www.jma.go.jp/bosai/forecast/data/overview_forecast/{code}.json"
+    overview_forecast_url = (
+        f"https://www.jma.go.jp/bosai/forecast/data/overview_forecast/{code}.json"
+    )
     forecast_url = f"https://www.jma.go.jp/bosai/forecast/data/forecast/{code}.json"
 
     resp = requests.get(overview_forecast_url)
